61d2741d3d16c06fd53f0a93f6d46fed4366c516
Amed Izaguirre <amedif87plus@gmail.com>
2025-06-02 23:56:55 -0400

    Propuesta inicial ValautionCode

--- src/3.Domain/Domain/BookingCode/BookingCode.cs
+++ src/3.Domain/Domain/BookingCode/BookingCode.cs
@@ -4,33 +4,7 @@ namespace Domain.BookingCode
 {
     public class BookingCode
     {
-        public DateTime Expirationdate { get; set; }
         public required string ValuationCode { get; set; }
-        public required string BookingToken { get; set; }
-        public decimal[]? amountBeforeTax { get; set; }
-        public decimal[]? amountAfterTax { get; set; }
-    }
-
-    public class Fee
-    {
-        public Daterange dateRange { get; set; }
-        public Fee1 fee { get; set; }
-    }
-
-    public class Daterange
-    {
-        public string startDate { get; set; }
-        public string endDate { get; set; }
-    }
-
-    public class Fee1
-    {
-        public string name { get; set; }
-        public string type { get; set; }
-        public int amount { get; set; }
-        public string amountType { get; set; }
-        public string chargeType { get; set; }
-        public string paymentType { get; set; }
-        public int effectivePerson { get; set; }
+        public required string BookCode { get; set; }       
     }
 }
--- src/3.Domain/Domain/ValuationCode/ValuationCode.cs
+++ src/3.Domain/Domain/ValuationCode/ValuationCode.cs
@@ -2,15 +2,8 @@
 {
     public class ValuationCode
     {
-        public required string RateId { get; set; }
-        public required string RoomId { get; set; }
-        public required string MealPlan { get; set; }
-        public required string CheckIn { get; set; }
-        public required int Nights { get; set; }
-        public required string AccommodationCode { get; set; }
-        public required IList<Tuple<int,IList<int>>> RoomCandidates { get; set; } //RoomRef, Pax ages
-        public string? Language { get; set; }
-        public string? Currency { get; set; }
-        public string? Nationality { get; set; }
+        public string EstablishmentId { get; set; }
+        public string[] BokingCode { get; set; }
+        public int[] RoomsRef { get; set; }
     }
 }
